111b22ac856734776ab02d4e7e8a9244fe57e65f
matheusxds123 <101800697+matheusxds123@users.noreply.github.com>
2023-06-02 09:23:39 -0300

    Inicialização do projeto

---
+++ src/main/java/com/matheusxavier/curso_spring_mongodb/CursoSpringMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.matheusxavier.curso_spring_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheusxavier/curso_spring_mongodb/CursoSpringMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusxavier.curso_spring_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
